ee76d878c7eef433203bd8766d8e2cf84b4f7ecf
Haru-tan <Haru-tan@users.noreply.github.com>
2020-09-23 19:39:03 +0100

    Match Product ID to DDRC-24

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
-	// Use Vendor and Product ID representing the MiniDSP 2x4HD
+	// Use Vendor and Product ID representing the MiniDSP DDRC-24
 	USB_VID: 0x2752,
-	USB_PID: 0x0011,
+	USB_PID: 0x0044,
 
 	INPUT_ANALOG: 0,
 	INPUT_TOSLINK: 1,
